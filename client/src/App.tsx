0743421c1d09d50128a105fd74eb44351ed4c4f7
hocsinhgioitoan <89916615+hocsinhgioitoan@users.noreply.github.com>
2023-04-01 11:24:08 +0700

    ✨ feat(App.tsx): add Hello World message to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
-
 function App() {
-
-  return (
-    <div>
-
-    </div>
-  )
+    return (
+        <div>
+            <h1
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100vw",
+                    height: "100vh",
+                    color: "white",
+                }}
+            >
+                Hello World
+            </h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
