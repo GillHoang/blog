2a1e28b88e42ef9bf135256a79613abecbc694f7
hocsinhgioitoan <89916615+hocsinhgioitoan@users.noreply.github.com>
2023-04-01 17:43:01 +0700

    💄 style(Navbar.tsx): refactor Navbar component to use Tailwind CSS classes and improve responsiveness

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,47 +2,50 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const NavbarLinks = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "About",
-    path: "/about",
-  },
-  {
-    name: "Contact",
-    path: "/contact",
-  },
-  {
-    name: "Blog",
-    path: "/blog",
-  },
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "About",
+        path: "/about",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+    {
+        name: "Blog",
+        path: "/blog",
+    },
 ];
 
 const Navbar = () => {
-  const baseClasses = "font-semibold text-lg";
+    const baseClasses = "font-semibold text-lg";
 
-  return (
-    <div className="fixed top-5 left-1/2 flex items-center gap-x-10 justify-between -translate-x-1/2 bg-backgroundColorLight max-w-[40vw] w-full px-20 py-5 rounded-full">
-      <Link to="/">
-        <h3 className="">hocsinhgioitoan</h3>
-      </Link>
-      <div className="flex items-center gap-x-10">
-        {NavbarLinks.map((link) => (
-          <NavLink
-            to={link.path}
-            key={link.name}
-            className={({ isActive }) =>
-              isActive ? `${baseClasses} text-white` : `${baseClasses} text-gray-400`
-            }
-          >
-            {link.name}
-          </NavLink>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="relative flex flex-wrap items-center justify-between px-4 py-2 sm:px-8 sm:py-4 md:px-16 md:py-6 lg:px-20 lg:py-8 xl:px-24">
+            <Link
+                to="/"
+                className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold"
+            >
+                hocsinhgioitoan
+            </Link>
+            <div className="flex items-center gap-x-10 mt-2 sm:mt-0 md:ml-auto">
+                {NavbarLinks.map((link) => (
+                    <NavLink
+                        to={link.path}
+                        key={link.name}
+                        className={({ isActive }) =>
+                            isActive ? `text-white` : `text-gray-400`
+                        }
+                    >
+                        {link.name}
+                    </NavLink>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
